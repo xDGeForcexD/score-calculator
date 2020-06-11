e8a1cb64326564f7469e9fc809fa1fed850ca278
xDGeForcexD <info@xdgeforcexd.eu>
2020-06-11 13:06:53 +0200

    Updates

---
+++ precache-manifest.bb577581b397e01a5f48332c36e8bd1e.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37e4294fe644a663562f4d2e1fd015d2",
+    "url": "/score-calculator/index.html"
+  },
+  {
+    "revision": "99556c76141c1fcf37d9",
+    "url": "/score-calculator/static/css/2.1b80c383.chunk.css"
+  },
+  {
+    "revision": "0cb43822597473069cc0",
+    "url": "/score-calculator/static/css/main.7968cca6.chunk.css"
+  },
+  {
+    "revision": "99556c76141c1fcf37d9",
+    "url": "/score-calculator/static/js/2.00b93a37.chunk.js"
+  },
+  {
+    "revision": "80378a430348c381484a739ad60035ba",
+    "url": "/score-calculator/static/js/2.00b93a37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cb43822597473069cc0",
+    "url": "/score-calculator/static/js/main.9d60dbc0.chunk.js"
+  },
+  {
+    "revision": "d37b32d984cdc87a8350",
+    "url": "/score-calculator/static/js/runtime-main.078fd211.js"
+  },
+  {
+    "revision": "a4d31128b633bc0b1cc1f18a34fb3851",
+    "url": "/score-calculator/static/media/Material-Design-Iconic-Font.a4d31128.woff2"
+  },
+  {
+    "revision": "b351bd62abcd96e924d9f44a3da169a7",
+    "url": "/score-calculator/static/media/Material-Design-Iconic-Font.b351bd62.ttf"
+  },
+  {
+    "revision": "d2a55d331bdd1a7ea97a8a1fbb3c569c",
+    "url": "/score-calculator/static/media/Material-Design-Iconic-Font.d2a55d33.woff"
+  },
+  {
+    "revision": "2ef8ba3410dcc71578a880e7064acd7a",
+    "url": "/score-calculator/static/media/fa-brands-400.2ef8ba34.woff"
+  },
+  {
+    "revision": "5bf145531213545e03ff41cd27df7d2b",
+    "url": "/score-calculator/static/media/fa-brands-400.5bf14553.svg"
+  },
+  {
+    "revision": "5e2f92123d241cabecf0b289b9b08d4a",
+    "url": "/score-calculator/static/media/fa-brands-400.5e2f9212.woff2"
+  },
+  {
+    "revision": "98b6db59be947f563350d2284fc9ea36",
+    "url": "/score-calculator/static/media/fa-brands-400.98b6db59.ttf"
+  },
+  {
+    "revision": "a7b95dbdd87e0c809570affaf366a434",
+    "url": "/score-calculator/static/media/fa-brands-400.a7b95dbd.eot"
+  },
+  {
+    "revision": "427d721b86fc9c68b2e85ad42b69238c",
+    "url": "/score-calculator/static/media/fa-regular-400.427d721b.woff"
+  },
+  {
+    "revision": "5eb754ab7dbd2fee562360528db4c3c0",
+    "url": "/score-calculator/static/media/fa-regular-400.5eb754ab.svg"
+  },
+  {
+    "revision": "65b9977aa23185e8964b36eddbce7a20",
+    "url": "/score-calculator/static/media/fa-regular-400.65b9977a.ttf"
+  },
+  {
+    "revision": "dcce4b7fbd5e895561e18af4668265af",
+    "url": "/score-calculator/static/media/fa-regular-400.dcce4b7f.eot"
+  },
+  {
+    "revision": "e6257a726a0cf6ec8c6fec22821c055f",
+    "url": "/score-calculator/static/media/fa-regular-400.e6257a72.woff2"
+  },
+  {
+    "revision": "418dad87601f9c8abd0e5798c0dc1feb",
+    "url": "/score-calculator/static/media/fa-solid-900.418dad87.woff2"
+  },
+  {
+    "revision": "46e7cec623d8bd790d9fdbc8de2d3ee7",
+    "url": "/score-calculator/static/media/fa-solid-900.46e7cec6.eot"
+  },
+  {
+    "revision": "49279363201ed19a840796e05a74a91b",
+    "url": "/score-calculator/static/media/fa-solid-900.49279363.svg"
+  },
+  {
+    "revision": "a7140145ebaaf5fb14e40430af5d25c4",
+    "url": "/score-calculator/static/media/fa-solid-900.a7140145.woff"
+  },
+  {
+    "revision": "ff8d9f8adb0d09f11d4816a152672f53",
+    "url": "/score-calculator/static/media/fa-solid-900.ff8d9f8a.ttf"
+  },
+  {
+    "revision": "665921072642ed354618b32af7425a22",
+    "url": "/score-calculator/static/media/ionicons.66592107.svg"
+  },
+  {
+    "revision": "7f9fdd5e7c0656fea97141765f3e0b50",
+    "url": "/score-calculator/static/media/ionicons.7f9fdd5e.eot"
+  },
+  {
+    "revision": "96f1c901c087fb64019f7665f7f8aca6",
+    "url": "/score-calculator/static/media/ionicons.96f1c901.woff2"
+  },
+  {
+    "revision": "c37ad37a3a23417b739ac3b297416201",
+    "url": "/score-calculator/static/media/ionicons.c37ad37a.woff"
+  },
+  {
+    "revision": "cfdc15225683b7529d6ba1e9d8a9be59",
+    "url": "/score-calculator/static/media/ionicons.cfdc1522.ttf"
+  }
+]);
